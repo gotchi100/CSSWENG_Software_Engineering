6322ee601d4cc94499aa32350416f21311c2113e
markadriii <69063424+markadriii@users.noreply.github.com>
2021-08-25 16:28:05 +0800

    views

---
+++ public/script/sales-customer-order-tracker.js
@@ -0,0 +1,78 @@
+$(document).ready(function () 
+{
+    let table = $("#sales_order_list_tracker_table").DataTable( 
+    {
+        columnDefs: [{
+                    orderable: false,
+                    className: "select-checkbox",
+                    targets: 0
+                    }],
+        select: {
+                style: "os",
+                selector: "td:first-child"
+                },
+        order: [
+                [1, "asc"]
+                ]
+    });
+
+    table.on("click", "th.select-checkbox", function() {
+    if ($("th.select-checkbox").hasClass("selected")) 
+    {
+        table.rows().deselect();
+        $("th.select-checkbox").removeClass("selected");
+        $("#delete_button").prop("disabled", true);
+    } 
+    else 
+    {
+        table.rows().select();
+        $("th.select-checkbox").addClass("selected");
+    }
+    }).on("select deselect", function() {
+        if (table.rows({ selected: true}).count() !== table.rows().count()) 
+        {
+            if(table.rows({ selected: true}).count() > 1)
+            {
+                $("th.select-checkbox").removeClass("selected");
+                $("#delete_button").prop("disabled", false);
+            }
+            else if (table.rows({ selected: true}).count() == 0)
+            {
+                $("th.select-checkbox").removeClass("selected");
+                $("#delete_button").prop("disabled", true);
+            }
+            else
+            {
+                $("th.select-checkbox").removeClass("selected");
+                $("#delete_button").prop("disabled", false);
+            }
+        } 
+        else 
+        {
+            $("th.select-checkbox").addClass("selected");
+            $("#delete_button").prop("disabled", true);
+        }
+    });
+
+    //an item in table is clicked
+    $("#sales_order_list_tracker_table tbody").on("click", "tr td", function () 
+    {
+
+        if ($(this).index() != 0) 
+        {    
+
+            $("#details_modal").modal("show");
+             
+        }
+    });
+
+    //add
+    $("#proceed_button").on("click", function () 
+    {
+        $("#details_modal").modal("hide");
+        $("#tracker_status_onroute_modal").modal("show");
+    });
+
+    
+
+});
