b743cd2b23fc2d8fab4b5dbb34f5d0cd4f9ee24b
markadriii <69063424+markadriii@users.noreply.github.com>
2021-09-11 18:49:38 +0800

    added adjusted date in sales to inventory

--- controllers/sales-controller.js
+++ controllers/sales-controller.js
@@ -90,7 +90,8 @@ const inventoryController = {
 						newquantity = result.Quantity - quantity;
 						
 						(function (productname, newquantity) {
-							db.Inventory.updateOne({ProductName: productname}, {Quantity: newquantity}, function(err, result){});
+							console.log("productname = " + productname + " newquantity = " + newquantity);
+							db.Inventory.updateOne({ProductName: productname}, {Quantity: newquantity, DateAdjusted: resultDate[0]}, function(err, result){});
 						})(productname, newquantity);
 					});
 				})(productname, quantity);
